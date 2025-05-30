90ed163c79b76d953b07c6703e1534b22a47fe47
Shinjan saha <shinjansaha02@gmail.com>
2025-05-30 12:19:44 +0530

    Update main.go

--- main.go
+++ main.go
@@ -1,3 +1,34 @@
+package mainAdd commentMore actions
+
+import (
+	"salary-management/config"
+	"salary-management/models"
+	"salary-management/routes"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/gin-gonic/gin"
+)
+
+
+func CORSMiddleware() gin.HandlerFunc {Add commentMore actions
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*") 
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(200)
+			return
+		}
+
+		c.Next()
+	}
+}
+
+
 func main() {
 	if os.Getenv("DATABASE_URL") == "" {
 		if err := godotenv.Load(); err != nil {
